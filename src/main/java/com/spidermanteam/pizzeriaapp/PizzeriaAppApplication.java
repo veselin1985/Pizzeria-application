6042c9613a8d1034795b3325fcda9ad0b3e4c3bc
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-07-26 16:05:25 +0300

    Create new project

---
+++ src/main/java/com/spidermanteam/pizzeriaapp/PizzeriaAppApplication.java
@@ -0,0 +1,12 @@
+package com.spidermanteam.pizzeriaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaAppApplication.class, args);
+    }
+}
